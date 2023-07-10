e74b9fd643ef053c9d0ea5889748c1bea77f0193
Pier-Olivier <pier-olivier.beaulieu.1@ens.etsmtl.ca>
2023-07-10 18:28:35 -0400

    TODO

--- src/MusicMediaCache.ts
+++ src/MusicMediaCache.ts
@@ -2,3 +2,5 @@ export {}
 
 // Map<id, MusicMedia>
 // Implement lazy load. If not in the map, call enpoint and store in map. Else, just return what is in the Map. 
+// if map[MusicMediaId] == null -> fetch https://localhost:8000/api/getmusicmedia?id=1 et ajouter a la map
+// return map[MusicMediaId]
