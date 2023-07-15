0f341ac77801c748e77afdd78d35e30d25fcb2f9
Pier-Olivier <pier-olivier.beaulieu.1@ens.etsmtl.ca>
2023-07-15 13:04:49 -0400

    interface comments

--- src/service/interface.ts
+++ src/service/interface.ts
@@ -45,27 +45,37 @@ export interface TokenCreation{
 }
 
 export interface Service {
-    // function to get connected account from metamask and return the user with the account address of the account 
+    // getConnectedAccount retrieves all connected account from metamask 
+    // and return the user with the first account address in the extension
     getConnectedAccount():Promise<User>
 
-    // Create Page - Artist creates a token from the Creation Page. This method also creates a listing
+    // ------------------      Creator Page  -----------------------------
+    // createTokens creates a token with the user address set as creator address. 
+    // This method mints tokens to the creator address. 
+    // This method also creates a listing with the seller address set as creator address. 
     createTokens(creatorAddress:string, name:string, numShares:number, price:number, div:number, initialTktPool: number, mp3:File, img:File): TokenCreation;
-    // Create Page - Artist visualizes the tokens he previously created
+
+    // getCreatedTokens retrieves all the tokens an artist previously created
     getCreatedTokens(creatorAddress: string): TokenCreation[];
-
-    // My Tokens Page - User sees the tokens he owns
+     
+     // ------------------   My Tokens Page ------------------------
+    // getOwnedTokens retrieves all the tokens a user owns
     getOwnedTokens(creatorAddress: string): TokenOwnership[];
-    // My Tokens Page - User adds a listing for a token he owns
+   
+    // addListing adds a listing for a token a user owns
     addListing(ownerAddress: string, tokenId: number, price: number, amount: number): Listing   
-    // My Tokens Page - User removes a listing for a token he previously created
+   
+    // removeListing removes a listing for a token a user previously created
     removeListing(ownerAddress: string, tokenId: number, price: number, amount: number): Listing   
-
-    // My Tokens Page - User sees the listings he created 
+    
+    // getUserListings retrieves all the listing created from the address provided
     getUserListings(ownerAddress: string): Listing[]
 
-    // MarketPage - User sees all the active listing created by other users
+    //------------------   Market Page  --------------------------
+    // getMarketListings retrieves all the active listing created by other users
     getMarketListings(): Listing[]
-    // MarketPage - User buys from a listing created by another user
+
+    // buy to buy from a listing created by another user
     buy(tokenId: number, buyerAddress: string, sellerAddress: string, amount: number, price: number): TokenOwnership
 
 }
